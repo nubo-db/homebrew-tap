e8cc7598965fb41f7d3ff406a4be29899c57deae
Martin Hicks <hello@martinhicks.net>
2026-04-02 14:59:46 +0000

    dynoxide 0.9.6

--- Formula/dynoxide.rb
+++ Formula/dynoxide.rb
@@ -1,3 +1,11 @@
+# Homebrew formula for Dynoxide
+# Place this in a tap repo (e.g., nubo-db/homebrew-tap) as Formula/dynoxide.rb
+# Install with: brew install nubo-db/tap/dynoxide
+#
+# NOTE: sha256 values are "PLACEHOLDER" in this template. They are replaced
+# automatically by the release CI workflow (.github/workflows/homebrew.yml)
+# when a new version is published. Do not fill them in manually.
+
 class Dynoxide < Formula
   desc "Fast, lightweight drop-in replacement for DynamoDB Local, backed by SQLite"
   homepage "https://github.com/nubo-db/dynoxide"
@@ -5,22 +13,22 @@ class Dynoxide < Formula
   license any_of: ["MIT", "Apache-2.0"]
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/nubo-db/dynoxide/releases/download/v0.9.6/dynoxide-aarch64-apple-darwin.tar.gz"
-      sha256 "bdacb056fa8bee366dd0a12f8c885bb3bb36c91c259eaeb46de1846e6fa2f129"
-    elsif Hardware::CPU.intel?
-      url "https://github.com/nubo-db/dynoxide/releases/download/v0.9.6/dynoxide-x86_64-apple-darwin.tar.gz"
-      sha256 "89a6520f65f4e32d4bcd5ebafa879bf37ca56a31c66e2018f5853a427423a1b8"
+    on_arm do
+      sha256 "31ac7bddc808218b7f07c806372ae1030305895c9dea6f08bba5d55a58369a82"
+    end
+
+    on_intel do
+      sha256 "ffd95cfb8af3812616cba1566b0fc92db2e27f1573216b6a1bdfe69eb7de65e9"
     end
   end
 
   on_linux do
-    if Hardware::CPU.arm?
-      url "https://github.com/nubo-db/dynoxide/releases/download/v0.9.6/dynoxide-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "728d4d239d14ec2806b6fff5d4119c282733fd27017074297e2c7be9d2053471"
-    elsif Hardware::CPU.intel?
-      url "https://github.com/nubo-db/dynoxide/releases/download/v0.9.6/dynoxide-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "4609d35c2cbb43095fd4fe98e1d713c69af3639b62c9ef680d8a6ef9020e16bd"
+    on_arm do
+      sha256 "4aec8fa5eb6bfda48654bdf2dd34106fc6aeccbc21ef1b42bafa6eb9b1c7cd4f"
+    end
+
+    on_intel do
+      sha256 "e59b55366d08e69ac97137a188c935114a9e5e934a73e8c963eebd9f0ed914e3"
     end
   end
 
@@ -29,7 +37,6 @@ class Dynoxide < Formula
   end
 
   test do
-    assert_match "dynoxide", shell_output("#{bin}/dynoxide --version")
+    assert_match version.to_s, shell_output("#{bin}/dynoxide --version")
   end
 end
-
