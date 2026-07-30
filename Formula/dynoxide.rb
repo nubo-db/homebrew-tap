a4c02f86cca5c2e43703434078478eabb8f7205e
Martin Hicks <hello@martinhicks.net>
2026-07-30 21:28:21 +0000

    dynoxide 0.13.0

--- Formula/dynoxide.rb
+++ Formula/dynoxide.rb
@@ -1,26 +1,26 @@
 class Dynoxide < Formula
   desc "Fast, lightweight drop-in replacement for DynamoDB Local, backed by SQLite"
   homepage "https://dynoxide.dev"
-  version "0.12.0"
+  version "0.13.0"
   license any_of: ["MIT", "Apache-2.0"]
   on_macos do
     on_arm do
-      url "https://github.com/nubo-db/dynoxide/releases/download/v0.12.0/dynoxide-aarch64-apple-darwin.tar.gz"
-      sha256 "1c46987caceaf07a12b676c6a384f8b4c83107974a1849ec35a3f8ee5f8ee4c4"
+      url "https://github.com/nubo-db/dynoxide/releases/download/v0.13.0/dynoxide-aarch64-apple-darwin.tar.gz"
+      sha256 "08a6166447587b3347430e5dafe605a08b8988dc96044babac2903a2f9bbd1b0"
     end
     on_intel do
-      url "https://github.com/nubo-db/dynoxide/releases/download/v0.12.0/dynoxide-x86_64-apple-darwin.tar.gz"
-      sha256 "8b6523d89af01cb6a5f6a7ff7796e788651dbf82b75a81f42d5f8c166b7b60d9"
+      url "https://github.com/nubo-db/dynoxide/releases/download/v0.13.0/dynoxide-x86_64-apple-darwin.tar.gz"
+      sha256 "4e92f2a49563bb5d2b14b609705548b00dbc2354311217e1d46a2ed6df234a54"
     end
   end
   on_linux do
     on_arm do
-      url "https://github.com/nubo-db/dynoxide/releases/download/v0.12.0/dynoxide-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "486939fbf079bfe41b55bc39519e2867d7ead193be1ddedcb7e845945a81b5f4"
+      url "https://github.com/nubo-db/dynoxide/releases/download/v0.13.0/dynoxide-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "d04ad4765a1e72e7892a32c6ae4daaba400f240c04c7072e0fdcd2c88574d890"
     end
     on_intel do
-      url "https://github.com/nubo-db/dynoxide/releases/download/v0.12.0/dynoxide-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "b593b62341774eed0d300bfe3067d414746051f6395f1512e5ff2f63962fc61f"
+      url "https://github.com/nubo-db/dynoxide/releases/download/v0.13.0/dynoxide-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "0fba11ccb618ddd8bbec4e5a7c4a688e756514922122fe930b8f00484d79cdf3"
     end
   end
   def install
